5811836511c1df6431b2dc8a76f9dfc81c255d9a
Suralius01 <suralius01@gmail.com>
2025-03-19 13:13:46 +0300

    Update data.tsx

--- src/components/data/data.tsx
+++ src/components/data/data.tsx
@@ -8,19 +8,19 @@ export const data: Data[] = [
   {
     title: "Showcase 1",
     description:
-      "Created an engaging and visually captivating short-form podcast clip for “Dagmawi Babi” on YouTube and TikTok. Utilized dynamic jump cuts, bold animated captions, and strategic zooms to emphasize key moments and keep viewers engaged. ",
+      "Created an engaging and visually captivating short-form podcast clip for “Dagmawi Babi” on YouTube and TikTok. Utilized dynamic edits to emphasize key moments and keep viewers engaged.",
     source: "showcase1_qauarz",
   },
   {
     title: "Showcase 2",
     description:
-      "Created an engaging original graphic animation short video featuring Selam Tesfaye for TikTok, Instagram, and other social platforms. Focused on high-energy motion graphics, smooth transitions, and eye-catching typography to enhance storytelling.",
+      "Created an engaging original graphic animation short video featuring Selam Tesfaye for TikTok, Instagram, and other social platforms. Focused on high-energy motion graphicsand cool transitions.",
     source: "showcase2_oiroxf",
   },
   {
     title: "Showcase 3",
     description:
-      "Produced a high-impact podcast snippet for “Dagmawi Babi,” designed specifically for TikTok, YouTube Shorts, and other short-form video platforms. Focused on fast-paced editing techniques, including seamless cuts, engaging motion graphics, and strategically timed zoom-ins to highlight crucial talking points.",
+      "Produced a high-impact podcast snippet for “Dagmawi Babi,” designed specifically for TikTok, YouTube Shorts, and other short-form video platforms. Motion graphics styled clip edit for podcast.",
     source: "showcase3_no0syl",
   },
 ];
