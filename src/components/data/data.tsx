0ab051abbde93aeb52bb98bd455c5f3052fc8442
Suralius01 <suralius01@gmail.com>
2025-03-27 16:15:25 +0300

    Update data.tsx

--- src/components/data/data.tsx
+++ src/components/data/data.tsx
@@ -6,27 +6,27 @@ interface Data {
 
 export const data: Data[] = [
   {
-    title: "Showcase 1",
+    title: "Short-Form Video Showcase",
     description:
       "Created an engaging and visually captivating short-form podcast clip for “Dagmawi Babi” on YouTube and TikTok. Utilized dynamic edits to emphasize key moments and keep viewers engaged.",
     source: "showcase1_qauarz",
   },
   {
-    title: "Showcase 2",
+    title: "Long-Form Video Showcase",
+    description:
+      "Produced a high-impact podcast snippet for “Dagmawi Babi,” designed specifically for TikTok, YouTube Shorts, and other short-form video platforms. Motion graphics styled clip edit for podcast.",
+    source: "https://res.cloudinary.com/dumf5fs66/video/upload/v1743074717/videos/dkzgbcjsuhbob8aiiees.mp4",
+  },
+  {
+    title: "Short-Form Video Showcase",
     description:
       "Created an engaging original graphic animation short video featuring Selam Tesfaye for TikTok, Instagram, and other social platforms. Focused on high-energy motion graphicsand cool transitions.",
     source: "showcase2_oiroxf",
   },
   {
-    title: "Showcase 3",
+    title: "Short-Form Video Showcase",
     description:
-      "Produced a high-impact podcast snippet for “Dagmawi Babi,” designed specifically for TikTok, YouTube Shorts, and other short-form video platforms. Motion graphics styled clip edit for podcast.",
+      "Produced a high-Quality Documentary styled video for youtube channel Named <<Americana Untold>>. Energetic and beautiful transition footages and some fun edits included. this video are made for youtube.",
     source: "showcase3_no0syl",
-  },
-  {
-    title: "Showcase 4",
-    description:
-      "Produced a high-impact podcast snippet for “Dagmawi Babi,” designed specifically for TikTok, YouTube Shorts, and other short-form video platforms. Motion graphics styled clip edit for podcast.",
-    source: "dkzgbcjsuhbob8aiiees",
   },
 ];
