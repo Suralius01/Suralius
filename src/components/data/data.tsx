4c74a8d80126994b1c0ce28c12f76f721c96cb7f
Suralius01 <suralius01@gmail.com>
2025-05-07 18:13:10 +0300

    Update data.tsx

--- src/components/data/data.tsx
+++ src/components/data/data.tsx
@@ -6,27 +6,27 @@ interface Data {
 
 export const data: Data[] = [
   {
-    title: "Short-Form Video Showcase",
+    title: "Short-Form Video Edit - Sports",
     description:
       "Created an engaging and visually captivating short-form podcast clip for “Dagmawi Babi” on YouTube and TikTok. Utilized dynamic edits to emphasize key moments and keep viewers engaged.",
-    source: "showcase1_qauarz",
+    source: "test_project_rbvzpd",
   },
   {
-    title: "Long-Form Video Showcase",
+    title: "Long-Form Video Edits - Sunny V2 Style",
     description:
       "Produced a high-impact podcast snippet for “Dagmawi Babi,” designed specifically for TikTok, YouTube Shorts, and other short-form video platforms. Motion graphics styled clip edit for podcast.",
     source: "https://res.cloudinary.com/dumf5fs66/video/upload/v1743074717/videos/dkzgbcjsuhbob8aiiees.mp4",
   },
   {
-    title: "Short-Form Video Showcase",
+    title: "Short-Form Video Edit Podcast Clip",
     description:
       "Created an engaging original graphic animation short video featuring Selam Tesfaye for TikTok, Instagram, and other social platforms. Focused on high-energy motion graphicsand cool transitions.",
-    source: "showcase2_oiroxf",
+    source: "0411_1_wugtg5",
   },
   {
-    title: "Short-Form Video Showcase",
+    title: "Short-Form Video Edit - Story-telling",
     description:
       "Produced a high-Quality Documentary styled video for youtube channel Named <<Americana Untold>>. Energetic and beautiful transition footages and some fun edits included. this video are made for youtube.",
-    source: "showcase3_no0syl",
+    source: "Selam_Tesfaye_tyvhoz",
   },
 ];
